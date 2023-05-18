6d0474de8cf862a8058a425d231688c1969a5171
Azriel <azrihell.l@gmail.com>
2023-05-18 15:00:31 -0500

    updated src to be individual per id

--- src/card.tsx
+++ src/card.tsx
@@ -3,8 +3,8 @@ import { } from "react";
 
 const Card = (props: any) => {
 return (
-  <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-    <img alt= 'robots' src='https://robohash.org/Johndoe?200x200' />
+  <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+    <img alt= 'robots' src={`https://robohash.org/${props.id}?200x200`} />
     <div>
       <h2>{props.name}</h2>
       <p>{props.email}</p>
