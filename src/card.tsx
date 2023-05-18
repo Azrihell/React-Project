831b474951999f984ecb44f967efade11b59f775
Azriel <azrihell.l@gmail.com>
2023-05-18 13:57:35 -0500

    added src url

--- src/card.tsx
+++ src/card.tsx
@@ -4,7 +4,7 @@ import {  } from "react";
 const Card = () => {
 return (
   <div>
-    <img alt= 'robots' src='' />
+    <img alt= 'robots' src='https://robohash.org/Johndoe?200x200' />
     <div>
       <h2>John Doe</h2>
       <p>johndoe@gmail.com</p>
