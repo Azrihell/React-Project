f158646b21846e5e3b94fe9b962e6c46390769e2
Azriel <azrihell.l@gmail.com>
2023-05-18 13:45:14 -0500

    created component for cards

---
+++ src/card.tsx
@@ -0,0 +1,16 @@
+import {  } from "react";
+
+
+const Card = () => {
+return (
+  <div>
+    <img alt= 'robots' src='' />
+    <div>
+      <h2>John Doe</h2>
+      <p>johndoe@gmail.com</p>
+    </div>
+  </div>
+  );
+}
+
+export default Card;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Hello from './Hello.tsx'
+import Card from './Card'
 import './index.css'
 import 'tachyons'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Hello />
+    <Card/>
   </React.StrictMode>,
 )
