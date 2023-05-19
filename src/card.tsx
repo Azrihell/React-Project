1b19b2b6803df69a707624c0a2cf90b249e8be76
Azriel <azrihell.l@gmail.com>
2023-05-18 23:01:29 -0500

    added formating script, resolved any

--- src/card.tsx
+++ src/card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export const Card = (props: any) => {
+export const Card = (props: {name: string, email: string, id: number}) => {
   return (
     <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
       <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
