3a4f8bd06efe7995ebf917ff02086682ffb0f472
Azriel <azrihell.l@gmail.com>
2023-05-22 18:55:32 -0500

    added searchbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { CardList } from './CardList'
 import { robots } from './robots'
+import { SearchBox } from './SearchBox'
 
 export const App = () => {
   return (
-    <CardList/>
+    <div className='tc'>
+      <h1>RoboFriends!</h1>
+      <SearchBox/>
+      <CardList/>
+    </div>
   )
 }
---
+++ src/SearchBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SearchBox = () => {
+  return (
+    <div>
+      <input className='bg-lightest-blue dib br3 pa3 ma2' type='search' placeholder='Search Friends' />
+    </div>
+  )
+}
