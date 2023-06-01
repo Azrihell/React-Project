ad2b95a3702120889f8fcd941a1c4151a9e68aca
Azriel <azrihell.l@gmail.com>
2023-05-31 23:14:36 -0500

    Added margin round searchbox, made font bigger

--- src/Components/SearchBox.tsx
+++ src/Components/SearchBox.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const SearchBox = (props: {searchField: string, setSearchField: any}) => {
   return (
-    <div>
+    <div className='SearchBox'>
       <input value={props.searchField} className='bg-white br3 pa3 ma2 b--white-10 gray fw6' type='search' placeholder='Search Friends' onChange={(e:any) => props.setSearchField(e.target.value)} />
     </div>
   )
